a04a4d7c71c85d468ab4b62c2afc636a1e622094
Júlio César Schincariol Filho <julio.com@uol.com.br>
2016-04-05 16:13:47 -0300

    Update jogoDaVelha.js

--- JogodaVelha/jogoDaVelha.js
+++ JogodaVelha/jogoDaVelha.js
@@ -12,12 +12,105 @@ function mudar(celula){
 			cor = "white";
 		}
 		celula.innerHTML = atual;
-		celula.style.backgroundColor=cor;
-		verificar();
-	}
+		celula.style.backgroundColor=cor;	
+	
+		if(verificarLinhas() || verificarColunas() || verificarVertical())
+			reiniciar();
+		
+	}else
+		alert("Casa já ocupada!");	
 	
 }
 
+function verificarLinhas(){
+	/*
+	 * 0 1 2     0, 0+1, 0+2 , linha 0 : 0, 1, 2
+	 * 3 4 5	 1, 1+3, 1+6 , linha 3 : 3, 4, 5
+	 * 6 7 8	 2, 2+3, 2+6 , linha 6 : 6, 7, 8	
+	 * 
+	 * 
+	 * */
+	var tabela = document.getElementsByTagName("table")[0];
+	var elementos = tabela.getElementsByTagName("td");
+	for(var i = 0; i <  elementos.length; i+=3){ // i+=3 pular para próxima linha
+		if( elementos[i].innerHTML == "x" && elementos[i + 1].innerHTML == "x" && elementos[i + 2].innerHTML == "x"){
+			alert("Jogador X ganhou!");
+			return true;
+		}
+		if( elementos[i].innerHTML == "o" && elementos[i + 1].innerHTML == "o" && elementos[i + 2].innerHTML == "o"){
+			alert("Jogador O ganhou!");
+			return true;
+		}		
+	}
+	return false;
+}
+
+function verificarColunas(){
+	
+	/*
+	 * 0 1 2     0, 0+3, 0+6 , coluna 0: 0, 3, 6
+	 * 3 4 5	 1, 1+3, 1+6 , coluna 1: 1, 4, 7
+	 * 6 7 8	 2, 2+3, 2+6 , coluna 2: 2, 5, 8	
+	 * 
+	 * 
+	 * */
+	var tabela = document.getElementsByTagName("table")[0];
+	var elementos = tabela.getElementsByTagName("td");
+	for(var i = 0; i < elementos.length; i++){ //i++ pq as colunas vão de 1 em 1
+		if (i == 3) // verifiquei as 3 colunhas 0, 1, e 2 então sai do loop já que ninguém ganhou
+			break;
+		if( elementos[i].innerHTML == "x" && elementos[i + 3].innerHTML == "x" && elementos[i + 6].innerHTML == "x"){
+			alert("Jogador X ganhou!");
+			return true;
+		}
+		if( elementos[i].innerHTML == "o" && elementos[i + 3].innerHTML == "o" && elementos[i + 6].innerHTML == "o"){
+			alert("Jogador O ganhou!");
+			return true;
+		}		
+	}
+	return false;
+}
+
+function verificarVertical(){
+	//x vertical
+	if( document.getElementById("1").innerHTML == "x" 
+		&& document.getElementById("5").innerHTML == "x" 
+			&& document.getElementById("9").innerHTML == "x"){
+		alert("Jogador X ganhou!");
+		return true;
+	}
+	if( document.getElementById("3").innerHTML == "x" 
+		&& document.getElementById("5").innerHTML == "x" 
+			&& document.getElementById("7").innerHTML == "x"){
+		alert("Jogador X ganhou!");
+		return true;
+	}
+	//o vertical
+	if( document.getElementById("1").innerHTML == "o" 
+		&& document.getElementById("5").innerHTML == "o" 
+			&& document.getElementById("9").innerHTML == "o"){
+		alert("Jogador O ganhou!");
+		return true;
+	}
+	if( document.getElementById("3").innerHTML == "o" 
+		&& document.getElementById("5").innerHTML == "o" 
+			&& document.getElementById("7").innerHTML == "o"){
+		alert("Jogador O ganhou!");
+		return true;
+	}
+}
+
+function reiniciar(){
+	var tabela = document.getElementsByTagName("table")[0];
+	var elementos = tabela.getElementsByTagName("td");
+	for(var i = 0; i <  elementos.length; i++){	
+		elementos[i].innerHTML = "";
+		elementos[i].style.backgroundColor = "";
+	}	
+	atual = "o";	
+}
+
+/*
 function verificar(){
 	var ganhador = false;
 	var casa1 = document.getElementById("1");
@@ -30,8 +123,7 @@ function verificar(){
 	var casa8 = document.getElementById("8");
 	var casa9 = document.getElementById("9");
 	
-	/* linhas */
-	/* x */
+
 	if( casa1.innerHTML == "x" 
 		&& casa2.innerHTML == "x" 
 			&& casa3.innerHTML == "x"){
@@ -53,7 +145,7 @@ function verificar(){
 		ganhador = true;
 	}
 	
-	/* o */
+	
 	if( casa1.innerHTML == "o" 
 		&& casa2.innerHTML == "o" 
 			&& casa3.innerHTML == "o"){
@@ -75,11 +167,10 @@ function verificar(){
 		ganhador = true;
 	}
 	
-	/* colunas */
-	/* x */
+
 	if( casa1.innerHTML == "x" 
 		&& casa4.innerHTML == "x" 
-			&& casa5.innerHTML == "x"){
+			&& casa7.innerHTML == "x"){
 		alert("X ganhador!");
 		ganhador = true;
 	}
@@ -98,7 +189,7 @@ function verificar(){
 		ganhador = true;
 	}
 	
-	/* o */
+	
 	if( casa1.innerHTML == "o" 
 		&& casa4.innerHTML == "o" 
 			&& casa7.innerHTML == "o"){
@@ -120,8 +211,8 @@ function verificar(){
 		ganhador = true;
 	}
 	
-	/* transversal */
-	/* x */
+	
+	
 	if(casa1.innerHTML == "x" 
 		&& casa5.innerHTML == "x"
 			&& casa9.innerHTML == "x"){
@@ -134,7 +225,7 @@ function verificar(){
 		alert("X ganhador!");
 		ganhador = true;
 	}
-	/* o */
+	
 	if(casa1.innerHTML == "o" 
 		&& casa5.innerHTML == "o"
 			&& casa9.innerHTML == "o"){
@@ -160,7 +251,9 @@ function verificar(){
 	
 	
 }
+*/
 
+/*
 function reiniciar(){
 	var casa1 = document.getElementById("1");
 	var casa2 = document.getElementById("2");
@@ -192,4 +285,4 @@ function reiniciar(){
 	casa9.style.backgroundColor = "";
 	
 	atual = "o";
-}
+}*/
